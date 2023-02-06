a8a147658cd5749c708a865516ca7ce4e3602e7b
Miri Dlm <delomiremini@gmail.com>
2023-02-06 20:51:52 +0100

    Finished Register/Login/Logout forms and functionalities

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout, authenticate
 from .forms import RegisterForm
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
 
+@login_required(login_url='/login')
 def home(request):
     return render(request, 'accounts/home.html')
 
